bd78b674b32ad4345237af8c958a359a0fa843c8
Yury Sverba <yurysverba@gmail.com>
2021-04-08 17:16:05 +0300

    part 5 is worked

---
+++ app/src/main/java/com/yurysverba/svchat2/Adapter/UserAdapter.java
@@ -0,0 +1,68 @@
+package com.yurysverba.svchat2.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.yurysverba.svchat2.Model.User;
+import com.yurysverba.svchat2.R;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private final Context mContext;
+    private final List<User> mUsers;
+
+    public UserAdapter  (Context mContext, List<User> mUsers) {
+        this.mUsers = mUsers;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = mUsers.get(position);
+        holder.avatar_username.setText(user.getUsername());
+        if (user.getImageURL().equals("default")){
+            holder.avatar_image.setImageResource(R.mipmap.ic_launcher);
+        }else {
+            Glide.with(mContext).load(user.getImageURL()).into(holder.avatar_image);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView avatar_username;
+        public ImageView avatar_image;
+
+        public ViewHolder (View itemView){
+            super(itemView);
+
+            avatar_username = itemView.findViewById(R.id.avatar_username);
+            avatar_image = itemView.findViewById(R.id.avatar_image);
+
+        }
+    }
+}
