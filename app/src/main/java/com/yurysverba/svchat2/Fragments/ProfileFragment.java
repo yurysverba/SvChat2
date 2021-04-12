990b2e877c4e5f2f1e4389f0d6d08ef96c059174
Yury Sverba <yurysverba@gmail.com>
2021-04-12 10:32:39 +0300

    part 10 worked

---
+++ app/src/main/java/com/yurysverba/svchat2/Fragments/ProfileFragment.java
@@ -0,0 +1,63 @@
+package com.yurysverba.svchat2.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.yurysverba.svchat2.Model.User;
+import com.yurysverba.svchat2.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ProfileFragment extends Fragment {
+
+    CircleImageView image_profile;
+    TextView username;
+    DatabaseReference reference;
+    FirebaseUser fuser;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        image_profile = view.findViewById(R.id.profile_image);
+        username = view.findViewById(R.id.username);
+
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(fuser.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                username.setText(user.getUsername());
+                if (user.getImageURL().equals("default")) {
+                    image_profile.setImageResource(R.mipmap.ic_launcher);
+                } else {
+                    Glide.with(getContext()).load(user.getImageURL()).into(image_profile);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/yurysverba/svchat2/MainActivity.java
+++ app/src/main/java/com/yurysverba/svchat2/MainActivity.java
@@ -27,6 +27,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.yurysverba.svchat2.Fragments.ChatsFragment;
+import com.yurysverba.svchat2.Fragments.ProfileFragment;
 import com.yurysverba.svchat2.Fragments.UsersFragment;
 import com.yurysverba.svchat2.Model.User;
 
@@ -56,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPagerAdapter.addFragment(new ChatsFragment(),"Chats");
         viewPagerAdapter.addFragment(new UsersFragment(),"Users");
+        viewPagerAdapter.addFragment(new ProfileFragment(),"Profile");
 
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
